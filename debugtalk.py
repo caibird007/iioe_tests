73ff75e6bef2e41e52705d67ae130ae0beab632d
root <306807275@qq.com>
2020-05-25 21:27:05 +0800

    update

--- debugtalk.py
+++ debugtalk.py
@@ -15,6 +15,163 @@ def is_courses_open(course_list):
             return False
     return True
 
+def is_resources_pdf(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['formatCode'] != 'PDF':
+            return False
+    return True
+
+def is_resources_video(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['formatCode'] != 'VIDEO':
+            return False
+    return True
+
+def is_resources_word(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['formatCode'] != 'WORD':
+            return False
+    return True
+
+def is_resources_other(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['formatCode'] != 'OTHER':
+            return False
+    return True
+
+def is_resources_IIOE_DOCUMENTS(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['categoryCode'] != 'IIOE_DOCUMENTS':
+            return False
+    return True
+
+def is_resources_IIOE_TRAINING_MATERIALS(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['categoryCode'] != 'IIOE_TRAINING_MATERIALS':
+            return False
+    return True
+
+def is_resources_ICT_ED_PUBLICATIONS(resource_list):
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['categoryCode'] != 'ICT_ED_PUBLICATIONS':
+            return False
+    return True
+
+def is_in_cn_library_categrois(resource_list):
+    category_list = ["IIOE文档", "IIOE培训材料", "ICT/教育出版物"]
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['categoryName'] not in category_list:
+            return False
+    return True
+
+def is_in_fr_library_categrois(resource_list):
+    category_list = ["Documents de l'IIOE", "Ressouces de formation de l'IIOE", "Publications sur les TIC"]
+    for resource in resource_list:
+        if resource['categoryName'] not in category_list:
+            return False
+    return True
+
+def is_in_en_library_categrois(resource_list):
+    category_list = ['IIOE Documents','IIOE Training Materials', 'ICT-ED Publications']
+    if not resource_list:
+        return True
+    for resource in resource_list:
+        if resource['categoryName'] not in category_list:
+            return False
+    return True
+
+def is_networks_college(network_list):
+    if not network_list:
+        return True
+    for network in network_list:
+        if network['type'] != 'COLLEGE':
+            return False
+    return True
+
+def search_Shenzhen_Polytechnic_from_college(network_list, lang='zh-CN'):
+    if not network_list:
+        return True
+    for network in network_list:
+        if network['name'] in ['Shenzhen Polytechnic','深圳职业技术学院','Institut Polytechnique de Shenzhen']:
+            if lang == 'zh-CN':
+                if network['country'] == '中国':
+                    return True
+                else:
+                    return False
+            elif lang == 'en-US':
+                if network['country'] == 'China':
+                    return True
+                else:
+                    return False
+            elif lang == 'fr-FR':
+                if network['country'] == 'Chine':
+                    return True
+                else:
+                    return False
+            else:
+                return False
+ 
+    return False
+
+
+def search_cio_times_from_enterprise(network_list, lang='zh-CN'):
+    if not network_list:
+        return True
+    for network in network_list:
+        if network['name'] in ['CIO Times','CIO时代学院'] :
+            if lang == 'zh-CN':
+                if network['country'] == '奥兰群岛':
+                    return True
+                else:
+                    return False
+            elif lang == 'en-US':
+                if network['country'] == 'Aland Islands':
+                    return True
+                else:
+                    return False
+            elif lang == 'fr-FR':
+                if network['country'] == 'Îles Aland':
+                    return True
+                else:
+                    return False
+            else:
+                return False
+ 
+    return False
+
+def is_networks_normal(network_list):
+    if not network_list:
+        return True
+    for network in network_list:
+        if network['status'] != 'NORMAL':
+            return False
+    return True
+
+
+def is_networks_enterprise(network_list):
+    if not network_list:
+        return True
+    for network in network_list:
+        if network['type'] != 'ENTERPRISE':
+            return False
+    return True
+
 
 def is_lang_fr(str):
     lang = langid.classify(str)
